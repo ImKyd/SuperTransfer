be03f348acf97fb31b356b4c7d080a49fd2787cb
ImKyd <kralikmatej@seznam.cz>
2017-07-02 08:14:20 +0200

    Update Main.php

--- src/SuperTransfer/Main.php
+++ src/SuperTransfer/Main.php
@@ -3,6 +3,7 @@
 namespace SuperTransfer;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
 
 class Main extends PluginBase{
   
@@ -10,4 +11,15 @@ class Main extends PluginBase{
      $this->saveDefaultConfig();
   }
   
+  public function transfer($server, Player $p){
+    $cfg = $this->getConfig();
+    $servers = $cfg->get("Servers");
+    if(empty($server[$servers])){
+      $p->sendMessage("§cCould not find server $server");
+    }else{
+      $a = array_search($server, $servers);
+      if($a !== null){
+        $p->transfer($servers[$server], 19132); //TODO search port by Connector
+      }
+  }
 }
